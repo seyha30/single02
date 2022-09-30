509b0b10a8c02b1e2be6c1107316c8f53a5901a1
seyha <sinseyha1x@gmail.com>
2022-09-30 15:08:57 +0700

    12

---
+++ src/main/java/com/Single02Application.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Single02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Single02Application.class, args);
+	}
+
+}
